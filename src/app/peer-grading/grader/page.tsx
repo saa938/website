bdd566a5c2e94fa3a55b27037763da51bcbd9475
saa938 <ashays@outlook.com>
2025-12-13 22:08:10 -0800

    fix

--- src/app/peer-grading/grader/page.tsx
+++ src/app/peer-grading/grader/page.tsx
@@ -6,9 +6,9 @@ import {
   updateDoc,
   orderBy,
   query,
+  DocumentReference,
   deleteDoc,
-} from "firebase/firestore";
-import type { DocumentReference, DocumentData } from "firebase/firestore";
+  DocumentData,
 } from "firebase/firestore";
 import { useUser } from "@/components/hooks/UserContext";
 import { db } from "@/lib/firebase";
